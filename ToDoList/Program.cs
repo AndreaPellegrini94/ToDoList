9d035fed1dcc7433d5449e71f71ca6c46167f01c
Andrea Pellegrini <a.pellegrini@contrader.group>
2025-02-22 00:35:16 +0100

    secondo commit

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,32 +1,55 @@
 using ToDoList.Components;
 using System.Net.Http.Headers;
-
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Aggiungi i servizi per le sessioni
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
-// Add services to the container.
-builder.Services.AddSingleton<ToDoService>();
+// Aggiungi i servizi al container
+builder.Services.AddScoped<ToDoService>();
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddHttpClient(name: "ToDoList.Api");
+builder.Services.AddHttpClient<AuthService>("ToDoList.Api", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7264");
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configura il middleware
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+// Abilita le sessioni
+app.UseSession();
+
+// Aggiungi il token alla richiesta se esiste in sessione
+app.Use(async (context, next) =>
+{
+    var token = context.Session.GetString("Token");
+    if (!string.IsNullOrEmpty(token))
+    {
+        context.Request.Headers.Add("Authorization", "Bearer " + token);
+    }
+    await next();
+});
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
