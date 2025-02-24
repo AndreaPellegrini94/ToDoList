35befb3fdbc506a83085eda85c2803c1a440cf1b
Andrea Pellegrini <a.pellegrini@contrader.group>
2025-02-24 11:06:17 +0100

    secondo commit .3(pulsante di logout)

--- ToDoList/obj/Debug/net9.0/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net9.0/ToDoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+598627953c74da1f86180c54360b8e99176a1945")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bffce49b81d9289170706de2df82e4dc0c92b01c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
